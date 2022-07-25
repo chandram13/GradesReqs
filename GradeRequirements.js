fc56e0a07d70f6749e81e720f898e9840a920b62
Marvish <mschandr@uci.edu>
2022-07-25 10:43:24 -0700

    Initial commit

---
+++ GradeRequirements.js
@@ -0,0 +1,28 @@
+
+// Marvish Chandra
+
+// Requirements to Pass the Course
+
+switch(100 < x < 0) {
+    case 1:
+        x = 60 < x < 0;
+        document.getElementById("demo").innerHTML = "You have failed the course."
+        break;
+    case 2:
+        x = 80 < x < 70; {
+        document.getElementById("demo").innerHTML = "You have received a C in the course."}
+        break;
+    case 3:
+        x = 90 < x < 80; {
+        document.getElementById("demo").innerHTML = "You have received a B in the course." }
+        break;
+    case 4:
+        x = 100 < x < 90; {
+        document.getElementById("demo").innerHTML = "You have received an A in the course!"
+        break;
+    }
+    default:
+        x = 100 < x < 0; {
+        document.getElementbyId("demo").innerHTML = "Thank you for taking the course!" }
+        break;
+}
